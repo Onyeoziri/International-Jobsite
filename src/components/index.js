231dd5622ea4e5bd41c87d9bdb6113670d1fc11f
Aaron Hooper <aaronrls1984@gmail.com>
2022-12-05 17:16:32 -0500

    homepage with backend

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-
-export {default as NavBar} from './navBar/NavBar';
--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export {default as Home} from './home/Home';
-export {default as Login} from './login/Login';
-export {default as About} from './about/About';
-
-export {default as ErrorPage} from './errorPage/ErrorPage';
-
-
